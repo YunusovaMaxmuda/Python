876cb1e24f3dc2b18c1d5df8a14da8a2544dc2fb
YunusovaMaxmuda <maxisstat@gmail.com>
2025-08-10 21:12:54 +0500

    Create file.py

---
+++ lesson-1/homework/file.py
@@ -0,0 +1,24 @@
+# Kvadratning perimetri va yuzasi
+tomon = 5
+perimetr = 4*tomon
+yuza = tomon**2
+print(perimetr)
+print(yuza)
+# Doiraning uzunligi (aylanasi)
+diametr = 20
+uzunlik = 3,14*diametr
+print(uzunlik)
+# Ikkita sonning o‘rtacha qiymati
+a = 3
+b = 4
+ortacha = (a+b)/2
+print(ortacha)
+# Yigindi, kopaytma va kvadratlar
+yigindi = a+b
+kopaytma = a*b
+kvadrat_a = a**2
+kvadrat_b = b**2
+print(yigindi)
+print(kopaytma) == a*b
+print(kvadrat_a) == a**2
+print(kvadrat_b) == b**2
