660ea7a1b70304c91485d7491715a036b023568b
YunusovaMaxmuda <maxisstat@gmail.com>
2025-08-10 21:55:45 +0500

    Create file.py

---
+++ lesson-2/homework/file.py
@@ -0,0 +1,48 @@
+txt = 'MsaatmiazD'
+
+txt[0::2]
+txt = 'MsaatmiazD'
+
+txt[::-2]
+txt = "I'm John. I am from London."
+txt1 = "John is from London."
+
+print(txt1)
+text = "I love my country"
+print(text[::-1])
+word = "Aziza"
+if word.lower() == word.lower()[::-1]:
+    print("Palindrom")
+else:
+    print("Palindrom emas")
+  word = "Maxmuda"
+if word.lower() == word.lower()[::-1]:
+    print("Palindrom")
+else:
+    print("Palindrom emas")
+  fruits = ['apple','banan','cherry','pear', 'peach']
+print(fruits[2]) 
+list = [1,2,3,4,5]
+print(list)
+list = [1,2,3,4,5]
+print(list)
+list.append(['A','B','C'])
+list
+list1 = [1,2,3]
+list2 = [4,5,6]
+birlashtirmoq = list1+list2
+print(birlashtirmoq)
+numbers = [1,2,3,4,5]
+list = [numbers[0],numbers[2],numbers[4]]
+print(list)
+cities = ["Paris","London","Tokyo","Berlin","Tashkent"]
+if "Paris" in cities:
+    print("Yes")
+else:
+    print("No")
+  numbers = [1,2,3,4,5,6]
+dublicate = numbers*2
+print(dublicate)
+list = [1,2,3,4,5]
+list[0], list[-1] = list[-1], list[0]
+print(list)
