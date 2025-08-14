1652af24fc030e0b8a7d86a12b00ffacefd1dfc5
YunusovaMaxmuda <maxisstat@gmail.com>
2025-08-14 22:09:33 +0500

    Update file.py

--- lesson-2/homework/file.py
+++ lesson-2/homework/file.py
@@ -1,48 +1,90 @@
-txt = 'MsaatmiazD'
+#1 Age calculator
 
+name = input("Ismingizni kiriting: ")
+birth_year = int(input("Tug'ilgan yilingizni kiriting: "))
+current_year = 2025
+age = current_year - birth_year
+print(f"{name}, sizning yoshingiz {age} da.")
+#2 Extract car names
+txt = 'LMaasleitbtui'
+txt[1::2]
 txt[0::2]
+#3 Extract car names
 txt = 'MsaatmiazD'
-
+txt[::2]
+txt = 'MsaatmiazD'
 txt[::-2]
-txt = "I'm John. I am from London."
-txt1 = "John is from London."
+#4 Etract residence area
+txt = "I'am John. I am from London"
+words = txt.split()
+if "from" in words:
+    index = words.index("from")
+    print("Residence area:", words[index + 1])
+else:
+    print("Manzil topilmadi.")
+    #5 Reverse string
+text = input("Matn kiriting: ")
+reversed_text = text[::-1]
+print("Teskarisi:", reversed_text)
+#6 Count Vowels
+text = input("Matn kiriting: ")
+vowels = 'aeiouAEIOU'
+count = sum(1 for char in text if char in vowels)
+print("Unli harflar soni:", count)
 
-print(txt1)
-text = "I love my country"
-print(text[::-1])
-word = "Aziza"
-if word.lower() == word.lower()[::-1]:
-    print("Palindrom")
+#7 Find Maximum Value
+numbers = list(map(int, input("Raqamlarni vergul bilan kiriting: ").split(',')))
+print("Eng katta qiymat:", max(numbers))
+
+#8 Check Palindrome
+word = input("So'z kiriting: ")
+if word == word[::-1]:
+    print("Bu so'z palindrome.")
 else:
-    print("Palindrom emas")
-  word = "Maxmuda"
-if word.lower() == word.lower()[::-1]:
-    print("Palindrom")
+    print("Bu so'z palindrome emas.")
+word = input("So'z kiriting: ")
+if word == word[::-1]:
+    print("Bu so'z palindrome.")
 else:
-    print("Palindrom emas")
-  fruits = ['apple','banan','cherry','pear', 'peach']
-print(fruits[2]) 
-list = [1,2,3,4,5]
-print(list)
-list = [1,2,3,4,5]
-print(list)
-list.append(['A','B','C'])
-list
-list1 = [1,2,3]
-list2 = [4,5,6]
-birlashtirmoq = list1+list2
-print(birlashtirmoq)
-numbers = [1,2,3,4,5]
-list = [numbers[0],numbers[2],numbers[4]]
-print(list)
-cities = ["Paris","London","Tokyo","Berlin","Tashkent"]
-if "Paris" in cities:
-    print("Yes")
-else:
-    print("No")
-  numbers = [1,2,3,4,5,6]
-dublicate = numbers*2
-print(dublicate)
-list = [1,2,3,4,5]
-list[0], list[-1] = list[-1], list[0]
-print(list)
+    print("Bu so'z palindrome emas.")
+
+#9 Extract Email Domain
+email = input("Email manzilini kiriting: ")
+domain = email.split('@')[-1]
+print("Domen:", domain)
+
+
+#10 Generate Random Password
+import random
+import string
+
+length = 12
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for _ in range(length))
+print("Tasodifiy parol:", password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
